d3d15501f078e7f1bef5795a40de16ac3257827b
joanrig <github email address>
2019-05-27 20:32:05 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+let store = { drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this)
+  }
+}
+
+let passengerId = 0
+class Passenger {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.passengers.push(this)
+  }
+}
+
+let tripId = 0
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    store.trips.push(this)
+  }
+
+  setDriver(driver){
+    this.driverId = driver.id
+  }
+
+  setUser(passenter){
+    this.passengerId = passenger.id
+  }
+
+}
