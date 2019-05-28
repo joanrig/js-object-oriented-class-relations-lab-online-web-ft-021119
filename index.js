cf9cc2e1e9749a4c141924e0e0a9e998cb1d8c5b
joanrig <github email address>
2019-05-27 23:52:05 -0400

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ class Driver {
     this.name = name;
     store.drivers.push(this)
   }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => trip.passenger())
+  }
+
 }
 
 let passengerId = 0
@@ -16,6 +26,16 @@ class Passenger {
     this.name = name;
     store.passengers.push(this)
   }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+
+  drivers(){
+    return this.trips().map(trip => trip.driver())
+  }
 }
 
 let tripId = 0
@@ -23,14 +43,21 @@ class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
     store.trips.push(this)
-  }
-
-  setDriver(driver){
     this.driverId = driver.id
-  }
-
-  setUser(passenter){
     this.passengerId = passenger.id
   }
+
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    })
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    })
+  }
+
 
 }
